57458413fc7103e110492692b2426ef058263dd5
harison008 <harison.avelino@escola.pr.gov.br>
2026-06-08 12:28:26 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Registra no console do navegador que o ecossistema foi carregado
+console.log("Plataforma 'Do Grão ao Som' carregada com sucesso.");
+
+// Seleciona o botão através do ID dele
+const botaoInteragir = document.getElementById("btn-interagir");
+
+// Adiciona um evento de clique ao botão de forma limpa
+botaoInteragir.addEventListener("click", function() {
+    alert("Incrível ver a tecnologia salvando o planeta, né? Viva a música sustentável!");
+});
